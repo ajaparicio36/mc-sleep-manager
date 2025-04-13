ba2c3ffacb451db6356343cd039f5a4689537f7a
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2025-04-14 00:32:01 +0800

    feat: connect all features to main

--- lib/src/main/java/com/tatayless/sleepmanager/SleepManager.java
+++ lib/src/main/java/com/tatayless/sleepmanager/SleepManager.java
@@ -1,20 +1,63 @@
 package com.tatayless.sleepmanager;
 
+import com.tatayless.sleepmanager.commands.CommandManager;
+import com.tatayless.sleepmanager.config.ConfigManager;
+import com.tatayless.sleepmanager.listeners.PlayerListener;
+import com.tatayless.sleepmanager.managers.VoteManager;
+import com.tatayless.sleepmanager.utils.MessageUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SleepManager extends JavaPlugin {
-    
+
+    private ConfigManager configManager;
+    private VoteManager voteManager;
+    private MessageUtils messageUtils;
+
     @Override
     public void onEnable() {
+        // Initialize config
+        configManager = new ConfigManager(this);
+        configManager.loadConfig();
+
+        // Initialize message utils for localization
+        messageUtils = new MessageUtils(this);
+
+        // Initialize vote manager
+        voteManager = new VoteManager(this);
+
+        // Register commands
+        CommandManager commandManager = new CommandManager(this);
+        getCommand("sleepmanager").setExecutor(commandManager);
+        getCommand("sleepmanager").setTabCompleter(commandManager);
+
+        // Register event listeners
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+
         getLogger().info("SleepManager has been enabled!");
     }
-    
+
     @Override
     public void onDisable() {
+        // Save config on disable
+        configManager.saveConfig();
+
+        // Clear any ongoing votes
+        if (voteManager != null) {
+            voteManager.clearAllVotes();
+        }
+
         getLogger().info("SleepManager has been disabled!");
     }
-    
-    public boolean someLibraryMethod() {
-        return true;
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public VoteManager getVoteManager() {
+        return voteManager;
+    }
+
+    public MessageUtils getMessageUtils() {
+        return messageUtils;
     }
 }
